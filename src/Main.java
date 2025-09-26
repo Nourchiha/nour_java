694fa0051fea057ed16f327186bdf0b17efdec97
Nour CHIHA <Nour.Chiha@esprit.tn>
2025-09-26 11:49:22 +0100

    prozit3

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        Animal lion = new Animal("F", "Lion", 8, true);
+        // Création d'animaux
+        Animal lion = new Animal("Félin", "Lion", 8, true);
+        Animal perroquet = new Animal("Oiseau", "Perroquet", 2, false);
 
-        Zoo myZoo = new Zoo("Park", "Tunis", 29);
+        // Création d'un zoo
+        Zoo myZoo = new Zoo("Park", "Tunis", 1);
 
+        // Affichage du zoo
         myZoo.displayZoo();
+
+        // Affichage d’un animal
         lion.displayAnimal();
 
+        // Test toString()
         System.out.println(myZoo);
-        System.out.println(myZoo.toString());
-
         System.out.println(lion);
-        System.out.println(lion.toString());
+
+        System.out.println("Ajout Lion : " + myZoo.addAnimal(lion));
+        System.out.println("Ajout Perroquet : " + myZoo.addAnimal(perroquet));
+        // Affichage des animaux ajoutés
+        System.out.println("\n=== Animaux dans le zoo ===");
+        for (int i = 0; i < myZoo.nbrAnimals; i++) {
+            System.out.println(myZoo.animals[i]);
+        }
     }
 }
